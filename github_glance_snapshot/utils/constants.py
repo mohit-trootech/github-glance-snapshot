442b3206df8745bec9ec0a7faf0c9cc9dfe51647
Mohit Prajapat <mohit.prajapat@trootech.com>
2025-02-26 12:03:31 +0530

    [Initialized] Initialized project with added app glance

---
+++ github_glance_snapshot/utils/constants.py
@@ -0,0 +1,48 @@
+from django.utils.translation import gettext_lazy as _
+
+DD_MM_YYYY = "%d-%m-%Y"
+YYYY_MM_DD = "%Y-%m-%d"
+
+
+# Settings Constants
+# =====================================================
+class Settings:
+    """Settings Constants"""
+
+    ROOT_URLCONF = "conf.urls"
+    WSGI_APPLICATION = "conf.wsgi.application"
+    ASGI_APPLICATION = "conf.asgi.application"
+    LANGUAGE_CODE = "en-us"
+    TIME_ZONE = "Asia/Kolkata"
+    USE_I18N = True
+    USE_TZ = True
+    STATIC_URL = "static/"
+    STATIC_ROOT = "assets/"
+    STATIC_FILES_DIRS = "static/"
+    TEMPLATES_URLS = "templates/"
+    MEDIA_URL = "media/"
+    MEDIA_ROOT = "media/"
+    DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Email Configurations
+# =====================================================
+class EmailConfig:
+    """Email Configurations"""
+
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_HOST = "smtp.gmail.com"
+    PORT_465 = 465
+    PORT_587 = 587
+
+
+# Celery Configuration
+# =====================================================
+class CeleryConfig:
+    """Celery Configuration"""
+
+    CELERY_BROKER_URL = "redis://localhost:6379/0"
+
+
+class AppLabelsModel:
+    pass
